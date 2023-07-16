b3479d159c5f22315995abcd4774a37393ba0a9c
renpeng <renpeng@p1.com>
2023-07-16 16:47:30 +0800

    add

--- src/2023/07/w1_438.cc
+++ src/2023/07/w1_438.cc
@@ -1,5 +1,44 @@
-#include<iostream>
+#include <algorithm>
+#include <iostream>
+#include <string>
+#include <vector>
 
-int main(){
-    return 0;
+using namespace std;
+class Solution {
+public:
+  vector<int> findAnagrams(string s, string p) {
+    size_t p_size = p.length();
+    size_t s_size = s.length();
+
+    if (p_size > s_size) {
+      return {};
+    }
+
+    string sorted_p = p;
+    sort(sorted_p.begin(), sorted_p.end());
+
+    vector<int> res;
+    for (int i = 0; i <= (s_size - p_size); ++i) {
+      string temp = s.substr(i, p_size);
+      sort(temp.begin(), temp.end());
+      if (sorted_p == temp) {
+        res.push_back(i);
+      }
+    }
+    return res;
+  }
+};
+
+int main() {
+  Solution *solution = new Solution();
+
+  string s = "cbaebabacd";
+  string p = "abc";
+
+  vector<int> result = solution->findAnagrams(s, p);
+
+  for (auto v : result) {
+    cout << v << endl;
+  }
+  return 0;
 }
