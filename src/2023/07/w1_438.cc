c50622a58f3b177ac08e61f7a0b5c6602610b480
renpeng <renpeng@p1.com>
2023-07-16 17:28:51 +0800

    add

--- src/2023/07/w1_438.cc
+++ src/2023/07/w1_438.cc
@@ -29,7 +29,7 @@ public:
     return res;
   }
 
-  vector<int> findAnagrams1(string s, string p) {
+  vector<int> findAnagrams2(string s, string p) {
     size_t p_size = p.size();
     size_t s_size = s.size();
 
@@ -54,6 +54,56 @@ public:
       --s_count[s[i] - 'a'];
       ++s_count[s[i + p_size] - 'a'];
       if (s_count == p_count) {
+        ans.emplace_back(i + 1);
+      }
+    }
+
+    return ans;
+  }
+
+  vector<int> findAnagrams(string s, string p) {
+    size_t p_size = p.size();
+    size_t s_size = s.size();
+
+    if (p_size > s_size) {
+      return {};
+    }
+
+    vector<int> ans;
+    vector<int> count(26);
+
+    for (int i = 0; i < p_size; ++i) {
+      ++count[s[i] - 'a'];
+      --count[p[i] - 'a'];
+    }
+
+    int differ = 0;
+    for (auto c : count) {
+      if (c != 0) {
+        differ++;
+      }
+    }
+
+    if (differ == 0) {
+      ans.emplace_back(0);
+    }
+
+    for (int i = 0; i < s_size - p_size; ++i) {
+      if (count[s[i] - 'a'] == 1) {
+        --differ;
+      } else if (count[s[i] - 'a'] == 0) {
+        ++differ;
+      }
+      --count[s[i] - 'a'];
+
+      if (count[s[i + p_size] - 'a'] == -1) {
+        --differ;
+      } else if (count[s[i + p_size] - 'a'] == 0) {
+        ++differ;
+      }
+      ++count[s[i + p_size] - 'a'];
+
+      if (differ == 0) {
         ans.emplace_back(i + 1);
       }
     }
