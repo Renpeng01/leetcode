36e82522ed1c7936b8a2ebffb3be8fdff78e467f
renpeng <renpeng@p1.com>
2023-11-26 14:29:49 +0800

    贪心算法 121

---
+++ src/贪心/121.cc
@@ -0,0 +1,27 @@
+#include <algorithm>
+#include <iostream>
+#include <vector>
+
+class Solution {
+public:
+  int maxProfit(std::vector<int> &prices) {
+    int begin = 0;
+    int max = 0;
+    for (int i = 0; i < prices.size(); i++) {
+      if (prices[i] <= prices[begin]) {
+        begin = i;
+      } else {
+        max = std::max(max, prices[i] - prices[begin]);
+      }
+    }
+    return max;
+  }
+};
+
+int main() {
+
+  Solution s;
+  std::vector<int> test1 = {7, 1, 5, 3, 6, 4};
+  int res = s.maxProfit(test1);
+  std::cout << res << std::endl;
+}
