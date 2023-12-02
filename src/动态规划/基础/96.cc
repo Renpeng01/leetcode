4023e40d010436249140fd8628e4c89e8402c513
renpeng <renpeng@p1.com>
2023-12-02 20:09:50 +0800

    add

--- src/动态规划/基础/96.cc
+++ src/动态规划/基础/96.cc
@@ -22,6 +22,19 @@ public:
 
     return dp[n];
   }
+
+  // 只是代码上的优化
+  int numTrees2(int n) {
+    std::vector<int> dp(n + 1);
+    dp[0] = 1;
+    for (int i = 1; i <= n; i++) {
+      for (int j = 1; j <= i; j++) {
+        dp[i] += (dp[j - 1] * dp[i - j]);
+      }
+    }
+
+    return dp[n];
+  }
 };
 
 int main() {
