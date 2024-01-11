8f1f82f1507e4a2edea66489db88ee5e31b885db
renpeng <renpeng@p1.com>
2024-01-12 07:50:02 +0800

    add

--- src/哈希表/454.cc
+++ src/哈希表/454.cc
@@ -27,8 +27,8 @@ public:
     return ans;
   }
 
-  int fourSumCount1(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3,
-                    vector<int> &nums4) {
+  int fourSumCount(vector<int> &nums1, vector<int> &nums2, vector<int> &nums3,
+                   vector<int> &nums4) {
     unordered_map<int, int> map;
 
     for (int i = 0; i < nums1.size(); i++) {
@@ -37,14 +37,14 @@ public:
       }
     }
 
-    print(map);
+    // print(map);
 
     int res = 0;
     for (int i = 0; i < nums3.size(); i++) {
       for (int j = 0; j < nums4.size(); j++) {
-        // if (map.find(0 - (map[nums3[i] + nums4[j]])) != map.end()) {
-        //   res += map[0 - (map[nums3[i] + nums4[j]])];
-        // }
+        if (map.find(-nums3[i] - nums4[j]) != map.end()) {
+          res += map[-nums3[i] - nums4[j]];
+        }
       }
     }
     return res;
